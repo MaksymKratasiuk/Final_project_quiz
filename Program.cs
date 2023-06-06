1415c6c3937337478b5dee0b7ee0e41d1597efa9
MaksymKratasiuk <makshm1307@gmail.com>
2023-06-06 19:41:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Final_project_quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
