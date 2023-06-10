9b1717f307e831c08f139918af39bf8f910be83b
MaksymKratasiuk <makshm1307@gmail.com>
2023-06-10 15:33:46 +0300

    did the class for quiz

--- Program.cs
+++ Program.cs
@@ -4,6 +4,12 @@
     {
         static void Main(string[] args)
         {
+            Quiz_Question question = new Quiz_Question();
+            question.LoadRandomQuestion();
+            Console.WriteLine(question.Question);
+
+            Console.WriteLine(question.Answer );
+        
         }
     }
 }
---
+++ Quiz_Question.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_project_quiz
+{
+    internal class Quiz_Question
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+
+        public void LoadRandomQuestion()
+        {
+            
+            // Зчитуємо всі рядки з файлу
+            string[] lines = File.ReadAllLines("D:\\вікторина файли\\geography_easy.txt");
+
+            // Випадковим чином вибираємо питання зі списку
+            string randomLine = lines[new Random().Next(0, lines.Length)];
+
+            // Розділяємо питання та відповідь
+            string[] parts = randomLine.Split(':');
+
+            if (parts.Length >= 2)
+            {
+                // Перша частина - питання
+                Question = parts[0].Trim();
+
+                // Друга частина - відповідь
+                Answer = parts[1].Trim();
+            }
+            else
+            {
+                // Якщо не вдалося розділити питання та відповідь, просто присвоюємо рядок у поле питання
+                Question = randomLine;
+            }
+        }
+
+    }
+}
